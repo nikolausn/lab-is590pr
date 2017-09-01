43626b6a66726d85c4701124080c6a19e93e9a2d
nikolausn <nikolaus.nova@gmail.com>
2017-09-01 08:30:07 -0500

    string operation

--- inclass/Week01/python-intro.py
+++ inclass/Week01/python-intro.py
@@ -1,6 +1,18 @@
+# ensure the space
 s = 'the quick brown fox'
 if 'q' in s:
-	print('yes')
+    print('yes')
 else:
-	print('no')
+    print('no')
 
+# change data type on a same variable name
+i = 0
+while i < 10:
+    print(i)
+    i += 1
+
+for j in range(2, 10, 2):
+    print(j)
+
+i = 'this is a string'
+print(i)
